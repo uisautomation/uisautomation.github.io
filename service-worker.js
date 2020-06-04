5d5a03490617e9c85499b4d3d4e7c097d9b68d19
GitHub Robot User <mike.bamford@gmail.com>
2020-06-04 15:48:36 +0000

    Generated react component documentation.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.cb045fbada811bed4c6e4a0b90fafe07.js"
+  "precache-manifest.d2cd165e4ba7e2ed3757d880935b9118.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
